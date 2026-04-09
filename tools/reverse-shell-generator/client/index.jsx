333ae80ebaf935e6b2052882f5f116927b905238
0xKudoX <laynekudo@gmail.com>
2026-04-08 19:46:36 -0700

    fix: reverse shell generator generate button left-aligned on mobile

--- tools/reverse-shell-generator/client/index.jsx
+++ tools/reverse-shell-generator/client/index.jsx
@@ -254,7 +254,7 @@ export default function ReverseShellGenerator() {
           </select>
         </div>
 
-        <button style={styles.button(!canGenerate)} onClick={handleGenerate} disabled={!canGenerate}>
+        <button style={{ ...styles.button(!canGenerate), alignSelf: isMobile ? 'flex-start' : 'flex-end' }} onClick={handleGenerate} disabled={!canGenerate}>
           Generate
         </button>
       </div>
